347739389a0572e7ddb5524d51be6108934bc358
yukiny0811 <yukiny0811@gmail.com>
2025-09-05 17:05:58 +0900

    initial commit

---
+++ Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version: 5.10
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "ColorExtensions",
+    platforms: [
+        .iOS(.v14),
+        .macOS(.v12),
+    ],
+    products: [
+        .library(
+            name: "ColorExtensions",
+            targets: ["ColorExtensions"]
+        ),
+    ],
+    targets: [
+        .target(
+            name: "ColorExtensions",
+            dependencies: []
+        ),
+    ]
+)
